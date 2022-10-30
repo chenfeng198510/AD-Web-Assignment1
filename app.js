8b24807e287d58ab0bec6c5f3a3b4569a2e26b49
chenfeng198510 <chenfeng198510@gmail.com>
2022-10-30 11:55:47 -0700

    original

--- app.js
+++ app.js
@@ -4,21 +4,28 @@ var indexRouter = require("./router/index");
 const { auth } = require('express-openid-connect');
 require('dotenv').config();
 
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASEURL,
+  clientID: process.env.CLIENTID,
+  issuerBaseURL: process.env.ISSUER,
+  clientSecret: process.env.CLIENTSECRET,
+  authorizationParams: {
+    response_type: 'code',
+    audience: 'http://localhost:5000',
+    scope: 'openid profile email'
+  }
+};
+ 
 app.set("views", "views");
 app.set("view engine", "ejs");
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: process.env.SECRET,
-    baseURL: process.env.BASEURL,
-    clientID: process.env.CLIENTID,
-    issuerBaseURL: process.env.ISSUERBASEURL
-  };
-  
   // auth router attaches /login, /logout, and /callback routes to the baseURL
-  app.use(auth(config));
-app.use(express.static("public"));
 
+app.use(express.static("public"));
+app.use(auth(config));
 app.use("/", indexRouter);
 
 var port = 3000;
--- router/index.js
+++ router/index.js
@@ -12,11 +12,20 @@ router.get("/", (req, res)=> {
 
 
 router.get("/secured", requiresAuth(), async (req, res)=> {
+    let data ={}
+    const {token_type, access_token} = req.oidc.accessToken;
     try{
-        const apiResponse = await axios.get('http://localhost:5000/public');
+        const apiResponse = await axios.get('http://localhost:5000/private',
+        {
+            headers: {
+                authorization: `${token_type} ${access_token}`
+            }
+        });
         data =apiResponse.data;
-    }catch(e){}
-
+    }catch(e){
+        console(e);
+    }
+        
     res.render("secured", { 
         title: "Secured Page",
         isAuthenticated: req.oidc.isAuthenticated(),
